8d164a855cd7f27d5239e8f716e511781ab847ac
Michael Hendricks <michael@ndrix.org>
2013-07-19 13:55:35 -0600

    Implement HTTP GET caching
    This makes no attempt to follow HTTP caching best practices.  It
    violates them blatantly because Google Tasks API headers tell us not
    to cache anything.  In this circumstance, the benefits seem worth
    costs of slightly stale data.

--- http.pl
+++ http.pl
@@ -4,11 +4,29 @@
 :- use_module(library(http/http_ssl_plugin)).  % support SSL
 :- use_module(library(readutil), [read_stream_to_codes/2]).
 
+:- dynamic http_get_cached/3.  % Url:atom, Expiry:epoch_seconds, Response
+
 http_get(Url, Response) :-
+    % can we use a cached response?
+
+    http_get_cached(Url, Expiry, Response0),
+    get_time(Now),
+    Now < Expiry,
+    !,
+    Response = Response0.
+http_get(Url, Response) :-
+    % nope, we must fetch a response over the network
+
+    retractall(http_get_cached(Url, _, _)),  % remove stale cache entries
     http_open(Url, Stream, []),
     read_stream_to_codes(Stream, Codes),
     create_response(Response, Codes),
-    close(Stream).
+    close(Stream),
+
+    % cache results for one minute
+    get_time(Now),
+    Expiry is Now + 60,
+    assertz(http_get_cached(Url, Expiry, Response)).
 
 http_post(Url, Form, Response) :-
     http_open(Url, Stream, [post(form(Form))]),
