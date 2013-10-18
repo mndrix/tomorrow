c66e69ceb72e342171cc658a1ed12ccba4bfee9e
Michael Hendricks <michael@ndrix.org>
2013-10-18 15:47:27 -0600

    Migrate to library(julian/lang/en)
    That module was factored out of this project, so we can use that
    instead.

--- prolog/tomorrow.pl
+++ prolog/tomorrow.pl
@@ -1,16 +1,10 @@
-:- use_module(library(clpfd)).
 :- use_module(library(func)).
-:- use_module(library(lambda)).
 :- use_module(library(readutil), [read_line_to_codes/2]).
 :- use_module(library(uri_qq)).
-:- use_module(library(dcg/basics), [ integer//1
-                                   , string//1
-                                   , string_without//2
-                                   ]).
+:- use_module(library(dcg/basics), [string_without//2]).
 
 :- use_module(library(julian)).
-:- use_module(library(julian/util), [dow_number/2]).
-:- use_module(library(julian/calendar/gregorian), [month_number/2]).
+:- use_module(library(julian/lang/en), [english_form//1]).
 :- [http].
 
 % tokens( refresh_token:atom
@@ -148,7 +142,7 @@ template_applicable(Task, retain) :-
     due(Task, ''),
     repeats(Task, English, _),
     English \= "",
-    ( phrase(repetition(Constraints), English) ->
+    ( phrase(english_form(Constraints), English) ->
         form_time([today|Constraints])
         %format('~w =>~n', [Constraints])
     ; % otherwise ->
@@ -186,134 +180,9 @@ repeat_on(Rule, [Line|Others]) -->
 end_of_content([],[]).
 
 
-% TODO make string//1 an argument like Separator
-% TODO so that I can call phrase(split(comma, day_of_week, Parts), Xs)
-split(Separator, [Part|Parts]) -->
-    string(Part),
-    Separator,
-    split(Separator, Parts).
-split(_, [Part], Part, []).
-
-
-comma --> " and ".
-comma --> " or ".
-comma --> ", ".
-comma --> ",".
-
-
-within --> " during ".
-within --> " in ".
-within --> " of ".
-
-ordinal(N) -->
-    ordinal_short(N).
-ordinal(N) -->
-    ordinal_long(N).
-
-ordinal_long(1) --> "first".
-ordinal_long(2) --> "second".
-ordinal_long(3) --> "third".
-ordinal_long(4) --> "fourth".
-ordinal_long(5) --> "fifth".
-
-ordinal_short(N) -->
-    integer(N),
-    ( "th"; "st"; "nd"; "rd" ).
-
-
-parity(X) -->
-    "even",
-    !,
-    { X mod 2 #= 0 }.
-parity(X) -->
-    "odd",
-    !,
-    { X mod 2 #= 1 }.
-
-
-% True if Day is an atom representing the day of week named in Codes
-codes_dow(Codes, Day) :-
-    atom_codes(Atom, Codes),
-    downcase_atom(Atom, Day),
-    dow_number(Day, _).
-
-
-% True if Month is an atom representing the month named in Codes
-codes_month(Codes, Month) :-
-    atom_codes(Atom, Codes),
-    downcase_atom(Atom, Month),
-    month_number(Month, _).
-
-
-repetition(true) -->
-    "each day",
-    !.
-repetition(weekday) -->
-    "weekday",
-    !.
-repetition(dow(Day)) -->
-    string(Word),
-    end_of_content,
-    { codes_dow(Word, Day) },
-    !.
-repetition(dow(Days)) -->
-    split(comma, Words),
-    { maplist(codes_dow, Words, Days) },
-    !.
-repetition(month(Month)) -->
-    string(Word),
-    end_of_content,
-    { codes_month(Word, Month) },
-    !.
-repetition(month(Months)) -->
-    split(comma, Words),
-    { maplist(codes_month, Words, Months) },
-    !.
-repetition(gregorian(Y,M,_)) -->
-    string(Word),
-    { codes_month(Word, Month) },
-    { month_number(Month, M) },
-    " ",
-    integer(Y),
-    { Y > 999 },
-    !.
-repetition(gregorian(_,M,D)) -->
-    string(Word),
-    { codes_month(Word, Month) },
-    { month_number(Month, M) },
-    " ",
-    ordinal(D),
-    !.
-repetition(gregorian(Y,_,_)) -->
-    parity(Y),
-    " years",
-    !.
-repetition([A,B|T]) -->  % list of at least two elements (indexable)
-    { Forms = [A,B|T] },
-    split(within, Parts),
-    { length(Parts, N), N > 1 },
-    { maplist(\Part^Form^once(phrase(repetition(Form),Part)), Parts, Fs) },
-    !,
-    % optimization: constraints on the end are usually more concrete
-    { reverse(Fs, Forms) }.
-repetition(nth(-1,Form)) -->
-    "final ",
-    repetition(Form),
-    !.
-repetition(nth([N0,N1],Form)) -->
-    ordinal(N0),
-    comma,
-    ordinal(N1),
-    " ",
-    repetition(Form),
-    !.
-repetition(nth(N,Form)) -->
-    ordinal(N),
-    " ",
-    repetition(Form),
-    !.
 % personal rules below here
-repetition(month([september,october,november,december,january,february,march,april])) -->
+:- multifile julian_lang_en:english_form//1.
+julian_lang_en:english_form(month([september,october,november,december,january,february,march,april])) -->
     "school year".
 
 
