63190e780000316731deb8a6e4d3b8e54288f103
Michael Hendricks <michael@ndrix.org>
2014-04-22 10:38:52 -0600

    Replace http_open/3 with curl
    http_open/3 keeps having trouble verifying SSL certificates.  Curl has
    no such trouble, so I'm just calling out to the curl binary to get the
    job done.
    
    Eventually I need to figure out why http_open/3 is having such trouble.
    It's been a problem for this project and the readability_parser project.

--- prolog/http.pl
+++ prolog/http.pl
@@ -18,7 +18,13 @@ http_get(Url, Response) :-
     % nope, we must fetch a response over the network
 
     retractall(http_get_cached(Url, _, _)),  % remove stale cache entries
-    http_open(Url, Stream, []),
+    %http_open(Url, Stream, []),
+    process_create( path(curl)
+                  , [Url]
+                  , [ stderr(null)
+                    , stdout(pipe(Stream))
+                    ]
+                  ),
     read_stream_to_codes(Stream, Codes),
     create_response(Response, Codes),
     close(Stream),
@@ -34,7 +40,15 @@ http_post(Url, json(JSON), Response) :-
     atom_json_term(Codes, JSON, [as(codes)]),
     http_post(Url, codes('application/json', Codes), Response).
 http_post(Url, Data, Response) :-
-    http_open(Url, Stream, [post(Data)]),
+    %http_open(Url, Stream, [post(Data)]),
+    form_curl(Data, CurlOptions),
+    append(CurlOptions, ["-X", "POST", Url], ProcessOptions),
+    process_create( path(curl)
+                  , ProcessOptions
+                  , [ stderr(null)
+                    , stdout(pipe(Stream))
+                    ]
+                  ),
     read_stream_to_codes(Stream, Codes),
     create_response(Response, Codes),
     close(Stream).
@@ -42,16 +56,47 @@ http_post(Url, Data, Response) :-
 
 http_delete(Url, Response) :-
     % TODO replace override header with method(delete) once SWI supports it
+    /*
     http_open( Url
              , Stream
              , [ post(codes(""))
                , request_header('X-HTTP-Method-Override'='DELETE')
                , status_code(204)  % No Content
+               , cert_verify_hook(ssl_verify)
                ]
              ),
+    */
+    process_create( path(curl)
+                  , ["-X", "DELETE", Url]
+                  , [ stderr(null)
+                    , stdout(pipe(Stream))
+                    ]
+                  ),
     read_stream_to_codes(Stream, Codes),
-    create_response(Response, Codes),
-    close(Stream).
+    create_response(Response, Codes).
+
+% accept all SSL certificates
+ssl_verify( _SSL
+          , _ProblemCertificate
+          , _AllCertificates
+          , _FirstCertificate
+          , _Error
+          ).
+
+
+form_curl(codes(ContentType, Codes), Options) :-
+    format(string(Header), "Content-Type: ~s", [ContentType]),
+    string_codes(Body, Codes),
+    Options = [ "--header",      Header
+              , "--data-binary", Body
+              ].
+form_curl(form(Form), Options) :-
+    form_curl_(Form, Options).
+
+form_curl_([], []).
+form_curl_([K=V|Form], ["--data-urlencode", Pair|Curl]) :-
+    format(string(Pair), "~s=~s", [K,V]),
+    form_curl_(Form, Curl).
 
 
 % convert a raw list of codes into a structured response according
